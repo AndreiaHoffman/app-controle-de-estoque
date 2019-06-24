0348bd8ed9a0141525b255c8e4cef185618387a0
Bruno Ferreira Duarte <brunofduarte@outlook.com>
2019-06-24 19:52:14 -0300

    ajustes

--- Controle_estoque/www/js/banco.js
+++ Controle_estoque/www/js/banco.js
@@ -11,8 +11,7 @@ function errorDB(err) {
     alert("Erro: " + err);
 }
 
-function sucessDB() {
-}
+function sucessDB() { }
 
 //criação de tabela
 function createDB(tx) {
@@ -46,6 +45,7 @@ function cadastro_view_db(tx) {
     tx.executeSql('SELECT * FROM cadastro', [], cadastro_view_data, errorDB);
 }
 
+//Para Andreia: aqui na hora de vc passar o id como parametro, estava errado ali, eu só acertei o on click
 function cadastro_view_data(tx, results) {
     $("#produto_lista").empty();
     var len = results.rows.length;
@@ -54,8 +54,8 @@ function cadastro_view_data(tx, results) {
         $("#produto_lista").append("<div class='item white mark border-blue-grey-200 margin-button shadow'>" +
             "<tr class='produto_item_lista' id='produto_item_" + results.rows.item(i).id + "'>" +
             "<td class='info'><h3>" + results.rows.item(i).nome + "</h3>" +
-            "<p>Quantidade: <label class='text-grey'>" + results.rows.item(i).quantidade + "</label></p></td>"+
-            "<div class='right' style='margin-top:-45px;'><button class='right grey-700 icon-text cyan' onclick='cadastro_update_dados()'+ results.row.item(i).id +')'>" +
+            "<p>Quantidade: <label class='text-grey'>" + results.rows.item(i).quantidade + "</label></p></td>" +
+            "<div class='right' style='margin-top:-45px;'><button class='right grey-700 icon-text cyan' onclick='cadastro_update_dados(" + results.row.item(i).id + ")' > " +
             "<i class='ion-navicon-round'></i> Detalhes" +
             "</button></div>" +
             "</tr></div><div class='space'></div>");
@@ -63,8 +63,8 @@ function cadastro_view_data(tx, results) {
 
 }
 
-function AbrirID(id){
-	window.open("results.rows.item(i).id");
+function AbrirID(id) {
+    window.open("results.rows.item(i).id");
 }
 
 //função de voltar tela
@@ -73,30 +73,31 @@ function voltar() {
 }
 
 function estoque() {
-    
+
 }
 
-function cadastro_update_dados(produto_id){
+//Para Andreia: aqui eu uso esse local storage para gravar o id internamente e depois chamo a pagina
+function cadastro_update_dados(produto_id) {
+    localStorage.setItem("ProdutoId", produto_id);
     window.location.assign("Busca_estoque.html");
+    //var cad_nome_update = $("#produto_item_" + produto_id + ".info h3").html();
+    //var cad_quant_update = $("#produto_item_" + produto_id + ".info p").html();
 
-    var cad_nome_update = $("#produto_item_" + produto_id + ".info h3").html();
-    var cad_quant_update = $("#produto_item_" + produto_id +".info p").html();
-
-    $("#produto_id_update").val(produto_id);
-    $("#produto_nome_update").val(produto_nome_update);
-    $("#produto_descricao_update").val(produto_descricao_update);
+    //$("#produto_id_update").val(produto_id);
+    //$("#produto_nome_update").val(produto_nome_update);
+    //$("#produto_descricao_update").val(produto_descricao_update);
     // resto...
-  }
+}
 
-  function cadastro_update(){
+function cadastro_update() {
     db.transaction(cad_update_db, errorDB, sucessDB);
-  }
+}
 
-  function cad_update_db(tx){
+function cad_update_db(tx) {
     var cad_id_novo = $("#produto_id_update").val();
     var cad_nome_novo = $("#produto_nome_update").val();
     var cad_desc_novo = $("#produto_descricao_update").val();
 
-    tx.executeSql('UPDATE cadastro SET nome = "'+cad_nome_novo+'", descricao = "'+cad_desc_novo+'" WHERE id="'+cad_id_novo+'"');
-    
-  }
+    tx.executeSql('UPDATE cadastro SET nome = "' + cad_nome_novo + '", descricao = "' + cad_desc_novo + '" WHERE id="' + cad_id_novo + '"');
+
+}
