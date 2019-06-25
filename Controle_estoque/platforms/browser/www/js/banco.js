2a0bb59ce8ee1d95ee033306c22beb826d25b9fc
AndreiaHoffman <andreiahoffman@outlook.com>
2019-06-25 14:13:25 -0300

    função delete com alert

--- Controle_estoque/platforms/browser/www/js/banco.js
+++ Controle_estoque/platforms/browser/www/js/banco.js
@@ -11,8 +11,7 @@ function errorDB(err) {
     alert("Erro: " + err);
 }
 
-function sucessDB() {
-}
+function sucessDB() { }
 
 //criação de tabela
 function createDB(tx) {
@@ -37,7 +36,7 @@ function cadastro_view() {
     db.transaction(cadastro_view_db, errorDB, sucessDB);
 }
 
-//função mostrar dados - visualizar
+//função mostrar dados - visualizar no estoque
 function cadastro_view() {
     db.transaction(cadastro_view_db, errorDB, sucessDB);
 }
@@ -55,7 +54,7 @@ function cadastro_view_data(tx, results) {
             "<tr class='produto_item_lista' id='produto_item_" + results.rows.item(i).id + "'>" +
             "<td class='info'><h3>" + results.rows.item(i).nome + "</h3>" +
             "<p>Quantidade: <label class='text-grey'>" + results.rows.item(i).quantidade + "</label></p></td>" +
-            "<div class='right' style='margin-top:-45px;'><button class='right grey-700 icon-text cyan' onclick='cadastro_update_dados("+results.rows.item(i).id+")'>" +
+            "<div class='right' style='margin-top:-45px;'><button class='right grey-700 icon-text cyan' onclick='cadastro_update_dados(" + results.rows.item(i).id + ")' > " +
             "<i class='ion-navicon-round'></i> Detalhes" +
             "</button></div>" +
             "</tr></div><div class='space'></div>");
@@ -72,29 +71,47 @@ function voltar() {
     window.location.assign("index.html");
 }
 
+// funções de visualizar dados na tela de detalhes
+function detalhes_view() {
+    db.transaction(Detalhes, errorDB, sucessDB);
+}
 
-function cadastro_update_dados(produto_id) {
+function Detalhes(tx) {
+    var produtoId = localStorage.getItem('ProdutoId');
+    tx.executeSql('SELECT * FROM cadastro where id=' + produtoId, [], preenche_view_data, errorDB);
+}
+
+function preenche_view_data(tx, results) {
+    $("#produto_lista").empty();
+    var len = results.rows.length;
+
+    for (var i = 0; i < len; i++) {
+        $("#produto_id_update").val(results.rows.item(i).id);
+        $("#produto_nome_update").val(results.rows.item(i).nome);
+        $("#produto_descricao_update").val(results.rows.item(i).descricao);
+        $("#produto_quant_update").val(results.rows.item(i).quantidade);
+        $("#produto_preco_update").val(results.rows.item(i).preco);
+    }
+}
+
+function cadastro_update_dados(produto_id) { //botão de detalhes para puxar o id
+    localStorage.setItem("ProdutoId", produto_id);
     window.location.assign("Busca_estoque.html");
-
-    var produto_nome_update = $("#produto_item_" + produto_id + ".info h3").html();
-    var produto_quant_update = $("#produto_item_" + produto_id + ".info p").html();
-
-    $("#produto_id_update").val(produto_id);
-    $("#produto_nome_update").val(produto_nome_update);
-    // $("#produto_descricao_update").val(produto_descricao_update);
-    $("#produto_quant_update").val(produto_quant_update);
-    // resto...
 }
 
-function cadastro_update() {
-    db.transaction(cad_update_db, errorDB, sucessDB);
+//função de alterar
+function update_view() {
+    db.transaction(update, errorDB, sucessDB);
 }
 
-function cad_update_db(tx) {
+function update(tx) {
     var produto_id_novo = $("#produto_id_update").val();
     var produto_nome_novo = $("#produto_nome_update").val();
     var produto_desc_novo = $("#produto_descricao_update").val();
 
-    tx.executeSql('UPDATE cadastro SET nome = "' + produto_nome_novo + '", descricao = "' + produto_desc_novo + '" WHERE id="' + produto_id_novo + '"');
+    tx.executeSql('UPDATE cadastro SET nome = "'+produto_nome_novo+'", descricao = "'+produto_desc_novo+'" WHERE id = "'+produto_id_novo+'"');
 
 }
+
+
+
