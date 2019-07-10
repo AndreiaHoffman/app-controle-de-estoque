e39aa0e766bdc651465d520f2c5b9bf9733ae2cc
AndreiaHoffman <andreiahoffman@outlook.com>
2019-07-10 10:00:33 -0300

    testes de pdf

--- Controle_estoque/platforms/browser/www/js/banco.js
+++ Controle_estoque/platforms/browser/www/js/banco.js
@@ -15,15 +15,32 @@ function sucessDB() { }
 
 //criação de tabela
 function createDB(tx) {
-    tx.executeSql('CREATE TABLE IF NOT EXISTS cadastro (id INTEGER PRIMARY KEY, nome VARCHAR(500), descricao VARCHAR(500), quantidade NUM(3), preco NUM(5))');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS cadastro (id INTEGER PRIMARY KEY, nome VARCHAR(50), descricao VARCHAR(50), quantidade NUM(3), preco NUM(6))');
 }
+
+//mensagem de cadastro efetuado com sucesso
+function openToastTop() {
+    openToast({
+      message: 'Cadastro feito com sucesso!',
+      class: 'full text-big text-strong black-opacity-70 text-white',
+      position: 'top'
+    })
+  }
 
 // comando de inserir dados no banco
 function cadastro_insert() {
-    db.transaction(cadastro_insert_db, errorDB, sucessDB);
+    var nome = $("#nome_produto").val();
+
+    if(nome == "" || nome.trim().length < 0){
+        alert("O nome do produto é obrigatório!");
+    }else{
+        db.transaction(cadastro_insert_db, errorDB, sucessDB);
+        openToastTop();
+    }
 }
 
 function cadastro_insert_db(tx) {
+
     var nome = $("#nome_produto").val();
     var descricao = $("#desc_produto").val();
     var quantidade = $("#quant_produto").val();
@@ -42,7 +59,7 @@ function cadastro_view() {
 }
 
 function cadastro_view_db(tx) {
-    tx.executeSql('SELECT * FROM cadastro', [], cadastro_view_data, errorDB);
+    tx.executeSql('SELECT * FROM cadastro ORDER BY nome', [], cadastro_view_data, errorDB);
 }
 
 function cadastro_view_data(tx, results) {
@@ -50,12 +67,11 @@ function cadastro_view_data(tx, results) {
     var len = results.rows.length;
 
     for (var i = 0; i < len; i++) {
-        $("#produto_lista").append("<div class='item white mark border-blue-grey-200 margin-button shadow'>" +
+        $("#produto_lista").append("<div class='item white mark border-deep-purple-100 margin-button shadow'>" +
             "<tr class='produto_item_lista' id='produto_item_" + results.rows.item(i).id + "'>" +
             "<td class='info'><h3>" + results.rows.item(i).nome + "</h3>" +
             "<p>Quantidade: <label class='text-grey'>" + results.rows.item(i).quantidade + "</label></p></td>" +
-            "<div class='right' style='margin-top:-45px;'><button class='right grey-700 icon-text cyan' onclick='cadastro_update_dados(" + results.rows.item(i).id + ")' > " +
-            "<i class='ion-navicon-round'></i> Detalhes" +
+            "<div class='right' style='margin-top:-55px;'><button class='icon ion-navicon-round' style='font-size:35px; color:indigo;' onclick='cadastro_update_dados(" + results.rows.item(i).id + ")' > " +
             "</button></div>" +
             "</tr></div><div class='space'></div>");
     }
@@ -129,15 +145,15 @@ function deleteItem() {
             {
                 label: 'SIM',
                 class: 'red-900',
-                onclick: function(){
+                onclick: function () {
                     db.transaction(Delete, errorDB, sucessDB);
-                    closeAlert();
+                    window.location.assign("Estoque.html");
                 }
             },
             {
                 label: 'NÃO',
                 class: 'text-white',
-                onclick: function(){
+                onclick: function () {
                     closeAlert();
                 }
             }
@@ -161,3 +177,87 @@ function venda_view() {
         }, null);
     });
 }
+
+
+//mostrar produto para selecionar na venda
+function AbrirID_produto(id) {
+    window.open("results.rows.item(i).id");
+}
+
+function select() {
+    var id = $('#pesqsuisar').val();
+    AddProduto(id);
+}
+
+function AddProduto(id) {
+    db.transaction(function (transaction) {
+        transaction.executeSql('SELECT id,nome FROM cadastro where id=' + id, [], function (tx, results) {
+
+            $("#div_mostrar").append("<div class='list' id='produto_venda_" + results.rows.item(i).id + "'>" +
+                "<div class='item'>" +
+                "<h3 class='text-deep-purple-800'>" + results.rows.item(0).nome + "</h3>" +
+                "<div class='list' id='div_quant'>" +
+                "<div class='item icon ion-calculator icon-right'>" +
+                "<input type=number' min='1' value='1' id='quant_produto'>" +
+                "</div>" +
+                "</div>" +
+                "<div class='right'><button class='icon ion-minus-circled blue' style='padding: 10px;' onclick='remove_produto(" + results.rows.item(0).id + ");'></button></div>" +
+                "</div></div>");
+
+        });
+    });
+}
+
+function remove_produto(produto_id) {
+    $("#produto_venda_" + produto_id).remove();
+}
+
+//alteração do estoque para vendas
+function baixa_estoque(tx) {
+    var produtos = new Array();
+    var lista = $('#div_mostrar').children().length;
+    for (var i = 0; i < lista; i++) {
+        var data = new Object();
+        var filho = $("#div_mostrar").children().eq(i);
+        var id = filho[0].id;
+        data.id = id.split("_")[2];
+        data.quantidade = filho[0].children[0].children[1].children[0].children[0].value;
+        produtos.push(data);
+    }
+
+    var sql = '';
+    db.transaction(function (transaction) {
+        for (var i = 0; i < produtos.length; i++) {
+            transaction.executeSql('UPDATE cadastro SET quantidade = quantidade - ' + produtos[i].quantidade + ' WHERE id = ' + produtos[i].id);
+        }
+    });
+
+}
+
+function venda() {
+    alert({
+        title: 'Venda',
+        message: 'Deseja confirmar essa venda?',
+        class: 'deep-purple',
+        buttons: [
+            {
+                label: 'SIM',
+                class: 'deep-purple-900',
+                onclick: function(){
+                    baixa_estoque();
+                    voltar();
+                }
+                
+            },
+            {
+                label: 'NÃO',
+                class: 'text-white',
+                onclick: function () {
+                    closeAlert();
+                }
+            }
+        ]
+    });
+}
+
+
